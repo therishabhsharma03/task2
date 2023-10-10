d6abe0386ba15db7b291e49d0b0b5343c0150b45
rishabhsssharma <manyrishabh@gmail.com>
2023-10-11 02:10:35 +0530

    frst commit

--- src/components/basic.js
+++ src/components/basic.js
@@ -6,7 +6,9 @@ function Basic()
         <section>
             
         <div class="main">
-            <h1>Calculator</h1>
+            <h1>Calculator </h1>
+            <h6>By - Rishabh Sharma -- 21BCE11053</h6>
+            h
         </div>
         </section>
     )
