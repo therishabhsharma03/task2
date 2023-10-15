ab736ed355c3a4c462d31cd81634a2de70627c65
Rishabh Sharma <113721437+therishabhsharma03@users.noreply.github.com>
2023-10-15 20:05:36 +0530

    Update basic.js

--- src/components/basic.js
+++ src/components/basic.js
@@ -8,7 +8,7 @@ function Basic()
         <div class="main">
             <h1>Calculator </h1>
             <h6>By - Rishabh Sharma -- 21BCE11053</h6>
-            h
+            
         </div>
         </section>
     )
